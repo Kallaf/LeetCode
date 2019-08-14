95f8fde7883a5388dc8fea7eb517daed172bf72e
Kallaf <43083774+Kallaf@users.noreply.github.com>
2019-08-14 13:29:42 +0200

    Create invert-binary-tree.cs

---
+++ invert-binary-tree.cs
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode InvertTree(TreeNode root) {
+        if(root == null)return root;
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        InvertTree(root.left);
+        InvertTree(root.right);
+        return root;
+    }
+}
