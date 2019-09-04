b57da3b9c51a80099bd7d81758a1abd4d521dc67
Ahmed Elkalaf <kalaf1997@yahoo.com>
2019-09-04 21:04:59 +0200

    Update sliding-window-maximum.cpp

--- sliding-window-maximum.cpp
+++ sliding-window-maximum.cpp
@@ -3,16 +3,30 @@ public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         vector<int> maxs;
         if(nums.empty())return maxs;
-        priority_queue<pair<int,int>> pq;
-        for(int i=0;i<k;i++)
-          pq.push({nums[i],i});
-        maxs.push_back(pq.top().first);
+        deque<int> dq;
+        dq.push_back(0);
+        for(int i=1;i<k;i++)
+        {
+            if(nums[i] > nums[dq.back()])
+                dq.push_back(i);
+            else
+            {
+                while((!dq.empty()) && nums[i] >= nums[dq.front()])dq.pop_front();  
+                dq.push_front(i);
+            }   
+        }
+        maxs.push_back(nums[dq.back()]);
         for(int i=k;i<nums.size();i++)
         {
-            while((!pq.empty()) && pq.top().second <= i-k)
-                pq.pop();
-            pq.push({nums[i],i});
-            maxs.push_back(pq.top().first);
+            while((!dq.empty()) && dq.back() <= i-k)dq.pop_back();
+            if(dq.empty() || nums[i] > nums[dq.back()])
+                dq.push_back(i);
+            else
+            {
+                while((!dq.empty()) && nums[i] >= nums[dq.front()])dq.pop_front();  
+                dq.push_front(i);
+            }
+            maxs.push_back(nums[dq.back()]);
         }
         return maxs;
     }
