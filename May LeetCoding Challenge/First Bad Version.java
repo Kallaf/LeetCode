2264208c7260235fa5862118fdbb3034c629fcdd
Ahmed Elkalaf <kalaf1997@yahoo.com>
2020-05-01 12:14:57 +0200

    Create First Bad Version.java

---
+++ May LeetCoding Challenge/First Bad Version.java
@@ -0,0 +1,12 @@
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int l = 1,r = n;
+        while(l<r)
+        {
+            int mid = l + (r-l)/2;
+            if(isBadVersion(mid))r = mid;
+            else l = mid + 1;
+        }
+        return r;
+    }
+}
