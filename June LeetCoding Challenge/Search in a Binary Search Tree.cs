d260a6245e3909846521f967a2d1c8498df5192b
Ahmed Elkalaf <kalaf1997@yahoo.com>
2020-06-15 20:34:53 +0200

    Create Search in a Binary Search Tree.cs

---
+++ June LeetCoding Challenge/Search in a Binary Search Tree.cs
@@ -0,0 +1,8 @@
+public class Solution {
+    public TreeNode SearchBST(TreeNode root, int val) {
+        if(root == null)return null;
+        if(root.val == val)return root;
+        if(root.val > val)return SearchBST(root.left,val);
+        return SearchBST(root.right,val);
+    }
+}
